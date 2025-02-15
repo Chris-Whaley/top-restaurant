0ca739ca2eefef5eeb196bc44cc6dfe4fa8c6b74
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2025-02-15 13:47:37 -0500

    fix: cleaning code

--- src/about.js
+++ src/about.js
@@ -4,14 +4,12 @@
 
 // odium
 export default function createAbout() {
+  const content = document.getElementById("content");
+  content.innerHTML = "";
+  const about = document.createElement("div");
 
-    const content = document.getElementById('content');
-    content.innerHTML = "";
-    const about = document.createElement('div');
+  about.classList.add("welcome");
+  about.textContent = "About the Roshar Restaurant";
 
-    about.classList.add('welcome');
-    about.textContent = 'About the Roshar Restaurant';
-
-    content.appendChild(about);
-    
-};
+  content.appendChild(about);
+}
